f93daded6f0a4b33e098f17786eb0eec3d5b2907
Abdelrahman Salah <33458355+abdelrahmansalahkhamis@users.noreply.github.com>
2022-04-11 19:11:02 +0200

    split resource file from web service

--- StarredRepos/Services/WebService.swift
+++ StarredRepos/Services/WebService.swift
@@ -7,29 +7,6 @@
 
 import Foundation
 
-enum NetworkError: Error{
-    case decodingError
-    case domainError
-    case urlError
-}
-
-enum HttpMethod: String{
-    case get = "GET"
-    case post = "POST"
-}
-
-struct Resource<T: Codable> {
-    let url: URL
-    var httpMethod: HttpMethod = .get
-    var body: Data? = nil
-}
-
-extension Resource{
-    init(url: URL){
-        self.url = url
-    }
-}
-
 
 class WebService{
     static func load<T> (resource: Resource<T>, completion: @escaping(Result<T, NetworkError>) -> Void){
