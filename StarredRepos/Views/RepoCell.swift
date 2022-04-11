19bb5e081a961f8d2f2558806df3d72644b49de7
Abdelrahman Salah <33458355+abdelrahmansalahkhamis@users.noreply.github.com>
2022-04-11 15:17:14 +0200

    remove web service

--- StarredRepos/Views/RepoCell.swift
+++ StarredRepos/Views/RepoCell.swift
@@ -12,6 +12,7 @@ class RepoCell: UITableViewCell {
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
+    
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
